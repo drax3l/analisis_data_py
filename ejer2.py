083bc3028cd3c21870fd2ae46b7521477b0d04ab
drax3l <alexjeanpierquispesierra@gmail.com>
2025-01-31 08:17:59 -0500

    realizar cambios

---
+++ ejer2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 31 07:55:30 2025
+
+@author: usuario
+"""
+
+import pandas as pd
+import numpy as np
+datos= pd.read_csv('living.csv')
+print(datos.head())
+print(datos.info())
